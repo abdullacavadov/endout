69d7ff8b0e9090af84aa0d788889f2c0b8cfe7d5
Abdulla Javadov <abdullacavadov@gmail.com>
2026-09-22 11:22:57 +0400

    Add payment configuration, result page and migration

---
+++ App/inc/payment_config.example.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'base_url' => 'https://txpgtst.kapitalbank.az/api',
+    'username' => 'BURAYA_BIRBANK_TERMINAL_USERNAME',
+    'password' => 'BURAYA_BIRBANK_TERMINAL_PASSWORD',
+    'callback_url' => 'http://localhost/endout/App/api/payment/callback.php'
+];
