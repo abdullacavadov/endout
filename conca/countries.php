ec4d2767bc3bb8b7cd55c7a730220f91119107bd
Abdulla Javadov <abdullacavadov@gmail.com>
2026-09-24 00:56:59 +0400

    admin: enforce countries permission

--- conca/countries.php
+++ conca/countries.php
@@ -1,4 +1,4 @@
-<?php include("inc/config.php"); ?>
+<?php require_once("inc/config.php"); require_once("inc/admin_auth.php"); require_admin_permission($pdo, "countries"); ?>
 
 <!DOCTYPE html>
 <html lang="az">
